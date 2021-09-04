ab2a19ddf1adeca6fb955a5ca2ed55a6cd41c64f
Markus Ort <markus@ort.dev>
2021-09-04 20:16:24 +0200

    removed invalid credentials

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-  channelId: process.env.CHANNEL || '883648786264625162',
-  token: process.env.TOKEN || 'ODgzNjQ5NjUzNTU0NDM0MDQ4.YTNA3g.PIAYx7eqq-r5sPfg3wXT6tGxX4k',
+  channelId: process.env.CHANNEL || 'CHANNEL ID HERE',
+  token: process.env.TOKEN || 'BOT TOKEN HERE',
   fiaUrl: 'https://www.fia.com/documents/'
 }
 
