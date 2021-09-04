bdcfcec3a9eac2c374f3ae3d67980da9bfc0baae
Markus Ort <markus@ort.dev>
2021-09-04 14:38:04 +0200

    changed URL to the automatic redirect

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const config = {
   channelId: process.env.CHANNEL || '883648786264625162',
   token: process.env.TOKEN || 'ODgzNjQ5NjUzNTU0NDM0MDQ4.YTNA3g.PIAYx7eqq-r5sPfg3wXT6tGxX4k',
-  fiaUrl: 'https://www.fia.com/documents/season/season-2021-1108/championships/fia-formula-one-world-championship-14'
+  fiaUrl: 'https://www.fia.com/documents/'
 }
 
 module.exports = config
