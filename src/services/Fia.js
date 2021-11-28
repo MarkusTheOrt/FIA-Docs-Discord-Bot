8cd89e41ddc54f2cf1c0d21fd3fefcc41e2d9c3d
Markus Ort <markus@ort.dev>
2021-11-28 16:25:25 +0100

    Fixed minor thing

--- src/services/Fia.js
+++ src/services/Fia.js
@@ -19,7 +19,10 @@ const makeEmbed = (document, event, guild) => {
 };
 
 const updateDocuments = async () => {
-  const documents = Database.documents.find({ isNew: true });
+  const documents = Database.documents.find(
+    { isNew: true },
+    { sort: { date: 1 } }
+  );
   documents.forEach(async (document) => {
     const guilds = Database.guilds.find({ channel: { $gt: "" } });
     const event = await Database.events.findOne(new ObjectId(document.event));
@@ -78,80 +81,3 @@ Client.on("ready", () => {
   updateDocuments();
   setInterval(updateDocuments, Config.fetchInterval * 1000);
 });
-
-/** 
- const fetchDocuments = async () => {
-  // Fetching Documents from the FIA Website
-  fetch("https://www.fia.com/documents").then(async (request) => {
-    if (request.ok === false) {
-      Log.Warn(`FIA.com encountered ${request.status}`);
-      return;
-    }
-    const html = await request.text();
-    // Parse Website Body
-
-    await (async () => {
-      const parsedHtml = cheerio.load(html);
-      const event = parsedHtml(".event-title.active");
-      const eventTitle = event.text().trim();
-      const dbEvent = await Database.events.updateOne(
-        { name: eventTitle },
-        {
-          $set: {
-            name: eventTitle,
-            year: new Date().getUTCFullYear(),
-            threads: [],
-          },
-        },
-        { upsert: true }
-      );
-      const guilds = await Database.guilds.find();
-      const channel = await Client.channels.fetch("914319201571864647");
-      const embed = new MessageEmbed();
-      embed.setTitle("Qatar Grand Prix");
-      embed.setDescription(">> FIA Documents");
-      const thread = await channel.threads.create({
-        name: eventTitle,
-        reason: "FIA-Docs-Bot",
-      });
-      // Retrieve all link elements whose url ends with pdf.
-      const documents = parsedHtml(".event-title.active + ul a[href$=pdf]");
-      const newDocs = [];
-      documents.toArray().forEach((document) => {
-        const newDoc = {
-          event: eventTitle,
-          url: encodeURI(`https://www.fia.com${document.attribs.href}`),
-        };
-        const title = parsedHtml(`a[href="${document.attribs.href}"] .title`);
-        const stringDate = parsedHtml(`a[href="${document.attribs.href}"]`);
-        newDoc.title = title.text().trim();
-        newDoc.date = Moment(
-          stringDate.text().trim(),
-          "D.M.YY HH:mm",
-          "Europe/Berlin"
-        ).format();
-        if (
-          newDoc.title !== undefined &&
-          newDoc.date !== undefined &&
-          newDoc.url !== undefined &&
-          newDoc.event !== undefined
-        ) {
-          thread.send({ embeds: [makeEmbed(newDoc)] });
-        }
-      });
-      //console.log(newDocs);
-    })().catch((e) => Log.Stack(e.stack));
-  });
-};
-
-const makeEmbed = (document) => {
-  const embed = new MessageEmbed();
-  embed.color = 11615;
-  embed.setAuthor("FIA Document");
-  embed.setTitle(document.title);
-  embed.setURL(document.url);
-  embed.setThumbnail("https://static.ort.dev/fiadontsueme/fia_logo.png");
-  embed.setFooter(Moment(document.date).format("lll"));
-  return embed;
-};
- */
