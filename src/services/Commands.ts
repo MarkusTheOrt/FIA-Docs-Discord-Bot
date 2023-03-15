d009df000dc1cee8d2a345816968807e3f1f4cfd
F1 MultiViewer <f1multiviewer@gmail.com>
2023-03-15 20:26:29 +0100

    Add role mentions for new documents

--- src/services/Commands.ts
+++ src/services/Commands.ts
@@ -24,80 +24,208 @@ const commands = [
             .setName("target")
             .setDescription("Only Text Channels are Valid")
         )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("notify-role")
+        .setDescription(
+          "Set the Role that gets pinged when a new Document is posted."
+        )
+        .addRoleOption((option) =>
+          option
+            .setName("target")
+            .setDescription("The Role to mention when new documents are posted")
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("remove-notify-role")
+        .setDescription(
+          "Removes the Role that gets pinged when a new Document is posted."
+        )
     ),
 ];
 
 const registerCommands = () => {
+  Client.on("ready", async () => {
+    const rest = new REST({ version: "10" }).setToken(unwrap(Config.botToken));
+    rest
+      .put(Routes.applicationCommands(Config.discordAppId), {
+        body: [commands[0].toJSON()],
+      })
+      .then(() => Log.Info("Registered Application Commands"))
+      .catch(Log.Stack);
+  });
 
-Client.on("ready", async () => {
-  const rest = new REST({ version: "10" }).setToken(unwrap(Config.botToken));
-  rest
-    .put(Routes.applicationCommands(Config.discordAppId), {
-      body: [commands[0].toJSON()],
-    })
-    .then(() => Log.Info("Registered Application Commands"))
-    .catch(Log.Stack);
-});
+  Client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
 
-Client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
+    if (
+      interaction.commandName === "fia" &&
+      interaction.options.getSubcommand() === "channel"
+    ) {
+      await Try(interaction.deferReply({ ephemeral: true }));
 
-  if (interaction.commandName === "fia") {
-    await Try(interaction.deferReply({ ephemeral: true }));
+      const memberPerms = wrap(interaction.memberPermissions);
+      const guildId = wrap(interaction.guildId);
+      const channel = wrap(interaction.options.getChannel("target"));
 
-    const memberPerms = wrap(interaction.memberPermissions);
-    const guildId = wrap(interaction.guildId);
-    const channel = wrap(interaction.options.getChannel("target"));
+      if (!unwrap(memberPerms).has(Permissions.FLAGS.ADMINISTRATOR)) {
+        await Try(
+          interaction.editReply(
+            "You don't have the required Permissions to do that."
+          )
+        );
+        return;
+      }
 
-    if (!unwrap(memberPerms).has(Permissions.FLAGS.ADMINISTRATOR)) {
-      await Try(
-        interaction.editReply(
-          "You don't have the required Permissions to do that."
+      if (isNone(channel)) {
+        await Try(interaction.editReply("Channel argument not set or found."));
+      }
+
+      if (unwrap(channel).type === ChannelType.GuildText) {
+        await Try(
+          interaction.editReply("Only Text channels are valid options.")
+        );
+        return;
+      }
+
+      if (isNone(guildId)) {
+        await Try(
+          interaction.reply({
+            content: "Context is missing Guild ID",
+            ephemeral: true,
+          })
+        );
+        return;
+      }
+
+      const data = await Try(
+        Database.Guilds.updateOne(
+          { id: unwrap(guildId) },
+          { $set: { channel: unwrap(channel).id } }
         )
       );
-      return;
+
+      if (isNone(data)) {
+        await Try(interaction.editReply("Database Error"));
+        return;
+      }
+
+      if (unwrap(data).acknowledged === true) {
+        await interaction.editReply(
+          `FIA channel set to <#${unwrap(channel).id}>`
+        );
+      } else {
+        await interaction.editReply("Database Error.");
+      }
     }
 
-    if (isNone(channel)) {
-      await Try(interaction.editReply("Channel argument not set or found."));
-    }
+    if (
+      interaction.commandName === "fia" &&
+      interaction.options.getSubcommand() === "notify-role"
+    ) {
+      await Try(interaction.deferReply({ ephemeral: true }));
 
-    if (unwrap(channel).type === ChannelType.GuildText) {
-      await Try(interaction.editReply("Only Text channels are valid options."));
-      return;
-    }
+      const memberPerms = wrap(interaction.memberPermissions);
+      const guildId = wrap(interaction.guildId);
+      const role = wrap(interaction.options.getRole("target"));
 
-    if (isNone(guildId)) {
-      await Try(
-        interaction.reply({
-          content: "Context is missing Guild ID",
-          ephemeral: true,
-        })
+      if (!unwrap(memberPerms).has(Permissions.FLAGS.ADMINISTRATOR)) {
+        await Try(
+          interaction.editReply(
+            "You don't have the required Permissions to do that."
+          )
+        );
+        return;
+      }
+
+      if (isNone(role)) {
+        await Try(interaction.editReply("Role argument not set or found."));
+      }
+
+      if (isNone(guildId)) {
+        await Try(
+          interaction.reply({
+            content: "Context is missing Guild ID",
+            ephemeral: true,
+          })
+        );
+        return;
+      }
+
+      const data = await Try(
+        Database.Guilds.updateOne(
+          { id: unwrap(guildId) },
+          { $set: { role: unwrap(role).id } }
+        )
       );
-      return;
+
+      if (isNone(data)) {
+        await Try(interaction.editReply("Database Error"));
+        return;
+      }
+
+      if (unwrap(data).acknowledged === true) {
+        await interaction.editReply({
+          content: `FIA channel set to ping <@&${unwrap(role).id}>`,
+          allowedMentions: { roles: [] },
+        });
+      } else {
+        await interaction.editReply("Database Error.");
+      }
     }
 
-    const data = await Try(
-      Database.Guilds.updateOne(
-        { id: unwrap(guildId) },
-        { $set: { channel: unwrap(channel).id } }
-      )
-    );
+    if (
+      interaction.commandName === "fia" &&
+      interaction.options.getSubcommand() === "remove-notify-role"
+    ) {
+      await Try(interaction.deferReply({ ephemeral: true }));
 
-    if (isNone(data)) {
-      await Try(interaction.editReply("Database Error"));
-      return;
-    }
+      const memberPerms = wrap(interaction.memberPermissions);
+      const guildId = wrap(interaction.guildId);
 
-    if (unwrap(data).acknowledged === true) {
-      await interaction.editReply(
-        `FIA channel set to <#${unwrap(channel).id}>`
+      if (!unwrap(memberPerms).has(Permissions.FLAGS.ADMINISTRATOR)) {
+        await Try(
+          interaction.editReply(
+            "You don't have the required Permissions to do that."
+          )
+        );
+        return;
+      }
+
+      if (isNone(guildId)) {
+        await Try(
+          interaction.reply({
+            content: "Context is missing Guild ID",
+            ephemeral: true,
+          })
+        );
+        return;
+      }
+
+      const data = await Try(
+        Database.Guilds.updateOne(
+          { id: unwrap(guildId) },
+          { $unset: { role: "" } }
+        )
       );
-    } else {
-      await interaction.editReply("Database Error.");
+
+      if (isNone(data)) {
+        await Try(interaction.editReply("Database Error"));
+        return;
+      }
+
+      if (unwrap(data).acknowledged === true) {
+        await interaction.editReply({
+          content: `FIA channel will no longer ping anyone`,
+          allowedMentions: { roles: [] },
+        });
+      } else {
+        await interaction.editReply("Database Error.");
+      }
     }
-  }
-});
-}
+  });
+};
 
 export default registerCommands;
