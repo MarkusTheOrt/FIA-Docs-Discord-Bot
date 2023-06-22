003e01630a7a4756ed270a44ea67afa66690c946
Markus Ort <markus@ort.dev>
2023-06-22 20:12:08 +0200

    updates packages, fixes bug

--- src/utils/Try.ts
+++ src/utils/Try.ts
@@ -2,15 +2,26 @@ import Log from "./Log.js";
 import { none, Option, some } from "./Option.js";
 
 export default async <T>(
-  promise: Promise<T>
+    promise: Promise<T>
 ): Promise<Option<Exclude<T, null | undefined>>> => {
-  try {
-    const data = await promise;
-    if (data === null || data === undefined)
-      return none;
-    return some(data) as Option<Awaited<Exclude<T, null | undefined>>>;
-  } catch (e){
-    Log.Stack(e as string);
-    return none as Option<Exclude<T, null | undefined>>;
-  }
+    try {
+        const data = await promise;
+        if (data === null || data === undefined)
+            return none;
+        return some(data) as Option<Awaited<Exclude<T, null | undefined>>>;
+    } catch (e) {
+        Log.Stack(e as string);
+        return none as Option<Exclude<T, null | undefined>>;
+    }
 };
+
+async function TryHarder<T>(promise: Promise<T>): Promise<[T | null, any | null]> {
+    try {
+        const data = await promise;
+        return [data, null];
+    } catch (e) {
+        return [null, e];
+    }
+}
+
+export { TryHarder }
